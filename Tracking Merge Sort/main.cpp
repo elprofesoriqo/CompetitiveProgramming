aa078ef71d35f0f9e74a454ef1c37a3cf2d6ffb0
elprofesoriqo <igorjankowwski@gmail.com>
2024-10-20 15:24:50 +0200

    Sort

--- Tracking Merge Sort/main.cpp
+++ Tracking Merge Sort/main.cpp
@@ -2,50 +2,78 @@
 
 using namespace std;
 
-void MergeSort(int* A, int L, int R) {
-    if (L < R - 1) {
-        int M = (L + R) / 2;
+void printArray(const int* A, int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << A[i] << (i < n - 1 ? " " : "\n");
+    }
+}
 
-        MergeSort(A, L, M);
-        MergeSort(A, M, R);
 
-        cout << "[" << L << ":" << M << ":" << R << "] "
-             << A[M] << " ["
-             << (L < M ? A[L] : 0) << " "
-             << (M < R ? A[M] : 0)
-             << "]" << endl;
 
-        int leftSize = M - L;
-        int rightSize = R - M;
-
-        int* left = new int[leftSize];
-        int* right = new int[rightSize];
-
-        for (int i = 0; i < leftSize; i++)
-            left[i] = A[L + i];
-        for (int j = 0; j < rightSize; j++)
-            right[j] = A[M + j];
-
-        int i = 0, j = 0, k = L;
-
-        while (i < leftSize && j < rightSize) {
-            if (left[i] <= right[j]) {
-                A[k++] = left[i++];
-            } else {
-                A[k++] = right[j++];
-            }
+int findMin(const int* A, int start, int end) {
+    int minVal = A[start];
+    for (int i = start + 1; i <= end; ++i) {
+        if (A[i] < minVal) {
+            minVal = A[i];
         }
+    }
+    return minVal;
+}
 
-        while (i < leftSize) {
+void merge(int *A, int L, int M, int R, const int *original) {
+    int leftSize = M - L + 1;
+    int rightSize = R - M;
+
+    int* left = new int[leftSize];
+    int* right = new int[rightSize];
+
+    for (int i = 0; i < leftSize; ++i)
+        left[i] = A[L + i];
+    for (int i = 0; i < rightSize; ++i)
+        right[i] = A[M + 1 + i];
+
+    int Mval = original[M];
+    int leftMin = findMin(original, L, M);
+    int rightMin = findMin(original, M + 1, R);
+
+    if (R - L + 1 >= 3) {
+        cout << "[" << L << ":" << M << ":" << R << "] " << Mval
+             << " [" << leftMin << " " << rightMin << "]" << endl;
+    }
+
+    int i = 0, j = 0, k = L;
+    while (i < leftSize && j < rightSize) {
+        if (left[i] <= right[j]) {
             A[k++] = left[i++];
-        }
-
-        while (j < rightSize) {
+        } else {
             A[k++] = right[j++];
         }
+    }
 
-        delete[] left;
-        delete[] right;
+    while (i < leftSize) {
+        A[k++] = left[i++];
+    }
+
+    while (j < rightSize) {
+        A[k++] = right[j++];
+    }
+
+    delete[] left;
+    delete[] right;
+}
+
+void mergeSort(int* A, int L, int R, const int* original) {
+    if (L < R) {
+        int M = L + (R - L) / 2;
+
+        if (L < M) {
+            mergeSort(A, L, M, original);
+        }
+        if (M + 1 < R) {
+            mergeSort(A, M + 1, R, original);
+        }
+
+        merge(A, L, M, R, original);
     }
 }
 
@@ -53,24 +81,24 @@ int main() {
     int n;
     cin >> n;
 
-    int* sequence = new int[n];
-    for (int i = 0; i < n; i++) {
-        cin >> sequence[i];
+    int* A = new int[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> A[i];
     }
 
-    for (int i = 0; i < n; i++) {
-        cout << sequence[i] << " ";
+    printArray(A, n);
+
+    int* original = new int[n];
+    for (int i = 0; i < n; ++i) {
+        original[i] = A[i];
     }
-    cout << endl;
 
-    MergeSort(sequence, 0, n);
+    mergeSort(A, 0, n - 1, original);
 
-    for (int i = 0; i < n; i++) {
-        cout << sequence[i] << " ";
-    }
-    cout << endl;
+    printArray(A, n);
 
-    delete[] sequence;
+    delete[] A;
+    delete[] original;
 
     return 0;
 }
